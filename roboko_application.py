c327dad179ce7bd6230c11da898132216c3cc10a
富澤拓海 <tomizawatakumi@tomizawatakuminoMacBook-Pro.local>
2019-09-07 15:53:11 +0900

    sort miss

--- roboko_application.py
+++ roboko_application.py
@@ -6,7 +6,6 @@ from roboko_message.roboko_message import roboko
 def file_create():
     try:
         with open(roboko.csv_path,'x', encoding = 'utf-8') as f:
-            f.write('レストラン名,回数' + '\n')
             return 0
     except FileExistsError as e:
             return 1
@@ -15,20 +14,21 @@ def file_read(flag):
     if flag == 1:
         with open(roboko.csv_path) as csvfile:
             reader = csv.reader(csvfile)
-            hedder = next(reader)
-            for row in reader:
-                csvlist = list(row)
-        csvlist.sort(reversed = True)
-        for i in range(len(csvlist)):
-            print(roboko.roboko_like_resutaurant + csvlist[i][0])
-            user_like = input()
-            if user_like.lower() == 'yes' or user_like.lower() == 'y':
-                break
-            elif user_like.lower() == 'no' or user_like.lower() == 'n':
-                pass
-            else:
-                print('入力されている文字が違います。[Yes/No]')
-                continue
+            csvlist = [[]]
+            csvlist = [row for row in reader]
+            csvlist.sort(reverse = True)
+            print(csvlist)
+            for i in range(len(csvlist)):
+                print(roboko.roboko_like_resutaurant + csvlist[i][0] + 'です。')
+                print(roboko.do_you_like_resutaurant)
+                user_like = input()
+                if user_like.lower() == 'yes' or user_like.lower() == 'y':
+                    break
+                elif user_like.lower() == 'no' or user_like.lower() == 'n':
+                    pass
+                else:
+                    print('入力されている文字が違います。[Yes/No]')
+                    continue
         return csvlist
     elif flag == 0:
         pass
@@ -38,21 +38,23 @@ def file_read(flag):
 
 def file_write(resutaurant):
     with open(roboko.csv_path,'a') as f:
-        f.write(resutaurant + ',1')
+        f.write(resutaurant.title() + ',1' + '\n')
 
 def file_rewrite(csvlist):
     with open(roboko.csv_path,'w') as f:
         for i in range(len(csvlist)):
-            f.write(csvlist[i][0] + ',' + csvlist[i][1])
+            f.write(csvlist[i][0].title() + ',' + str(csvlist[i][1]) + '\n')
 
 def file_control(csvlist,resutaurant):
     for i in range(len(csvlist)):
-        if csvlist[i][0] == resutaurant:
+        if csvlist[i][0] == resutaurant.title():
             csvlist[i][1] = int(csvlist[i][1]) + 1
-            return csvlist,1
+            break
+        elif i == (len(csvlist) -1 ):
+            return csvlist,0
         else:
             pass
-    return csvlist,0
+    return csvlist,1
 
 def main():
     flag = file_create()
@@ -61,14 +63,15 @@ def main():
     csvlist = file_read(flag)
     print(user_name + roboko.where_restaurant_like)
     resutaurant = input()
-    csvlist,write_flag = file_control(csvlist,resutaurant)
-
-    if write_flag == 1:
-        file_rewrite(csvlist)
-    elif write_flag == 0:
-        file_write(resutaurant)
+    if flag == 1:
+        csvlist,write_flag = file_control(csvlist,resutaurant)
+        if write_flag == 1:
+            file_rewrite(csvlist)
+        else:
+            file_write(resutaurant)
     else:
-        sys.exit()
+        file_write(resutaurant)
+
     print(user_name + roboko.thank_you)
     print(roboko.goodby_message)
 
